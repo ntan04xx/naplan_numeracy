23bda77cb4ba97fad708432027c91e95cfeca30d
ntan04xx <nathantan04@gmail.com>
2024-07-07 01:29:28 +1000

    created elastic net and linear model with k-fold cross validation

--- TF School Profile.R
+++ TF School Profile.R
@@ -2,16 +2,18 @@ library(readxl)
 library(dplyr)
 library(tidyr)
 library(caret)
+library(glmnet)
 
 profile_data = read_excel("School Profile 2023.xlsx", sheet = 2)
 numeracy_data = read.csv("2023NAPLAN_scores_yr5.csv")
 location_data = read_excel("School Location 2023.xlsx", sheet = 2)
+location_subset_shared_data <- select(location_data, -`Calendar Year`, -`Location AGE ID`, -`School AGE ID`, -`School Name`, -Suburb, -State, -Postcode, -`School Sector`, -`School Type`, -`Campus Type`)
 metrics_data = read_excel("school_metrics_2023.xlsx", sheet = 2)
 
 school_data = inner_join(profile_data, metrics_data, by = "ACARA.SML.ID")
-school_data = inner_join(school_data, location_data, by = "ACARA.SML.ID")
+school_data = inner_join(school_data, location_subset_shared_data, by = "ACARA.SML.ID")
 school_data = inner_join(school_data, numeracy_data, by = "ACARA.SML.ID")
-school_data %>% filter(school_data$`Calendar Year.x` == 2023)
+school_data %>% filter(school_data$`Calendar Year` == 2023)
 school_data <- school_data %>%
   drop_na()
 summary(school_data)
@@ -29,5 +31,20 @@ set.seed(69420)
 k <- 10 # Number of folds for cross-validation
 train_control <- trainControl(method = "cv", number = k)
 
-naplan_model_knn <- train(NAPLAN ~ State.x + `School Sector.x` + ICSEA + sport_index + teach_edu_index + stu_atd + teach_tenure + student.teacher.ratio, data = school_data, method = "knn", trControl = train_control)
+# KNN
+formula <- NAPLAN ~ State + `School Sector` + ICSEA + sport_index + teach_edu_index + stu_atd + teach_tenure + student.teacher.ratio
+naplan_model_knn <- train(formula, data = school_data, method = "knn", trControl = train_control) # Need to separate into different datasets later
 print(naplan_model_knn)
+
+# Multiple linear
+naplan_model_linear <- train(formula, data = school_data, method = "lm", trControl = train_control)
+print(naplan_model_linear)
+
+# Elastic Net
+tune_grid <- expand.grid(
+  alpha = seq(0, 1, length = 20),
+  lambda = seq(0.001, 0.1, length = 20)
+)
+naplan_model_elastic <- train(formula, data = school_data, method = "glmnet", trControl = train_control, tuneGrid = tune_grid)
+print(naplan_model_elastic)
+
